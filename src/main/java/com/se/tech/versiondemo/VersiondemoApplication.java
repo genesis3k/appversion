3580032c3d5f23010308f6e57d77f3e1927822ea
Ehsan <ehsanmdali@gmail.com>
2020-08-16 17:12:51 +1000

    test included

--- src/main/java/com/se/tech/versiondemo/VersiondemoApplication.java
+++ src/main/java/com/se/tech/versiondemo/VersiondemoApplication.java
@@ -3,6 +3,8 @@ package com.se.tech.versiondemo;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,48 +12,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 
 public class VersiondemoApplication {
-	@Value("${git.tags}")
-    private String tags;
+	@Value("${git.closest.tag.name}")
+    private String tagName;
  
     @Value("${git.build.version}")
     private String buildVersion;
  
-    @Value("${git.commit.id}")
-    private String commitSHAId;
+    @Value("${git.commit.id.abbrev}")
+    private String commitSHAIdAbbrev;
+    private static final Logger LOG = LoggerFactory.getLogger(VersiondemoApplication.class);
     
 	public static void main(String[] args) {
 		SpringApplication.run(VersiondemoApplication.class, args);
 	}
-	
-	@RequestMapping(value = "/")
-	 public String versionInfo() {
-//	    return "Hello World";
-		
-		StringBuffer strBuff = new StringBuffer();
-		strBuff.append("Application info\n");
-		strBuff.append("tags:" + tags + ",\n");
-		strBuff.append("build version:" + buildVersion + ",\n");
-		strBuff.append("commit SHA Id:" + commitSHAId + "\n");
-		return strBuff.toString();
-	 }
-	
-	@RequestMapping("/version")
-    public Map<String, String> getCommitId() {
-        Map<String, String> result = new HashMap<>();
-        if (tags.isEmpty()) {
-        	tags = buildVersion;
-        }
-        result.put("version",tags);
-        result.put("lastcommitsha", commitSHAId);
-        result.put("description", "Demo to show Application version");
-        return result;
-    }
 	
 	@Bean
     public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
@@ -62,5 +42,47 @@ public class VersiondemoApplication {
         props.setIgnoreUnresolvablePlaceholders(true);
         return props;
     }
+	
+	//This endpoint on HTTP GET request, will respond with a JSON containing injected values from git.properties. 
+	//Below should be used if pure JSON format is required, without any additional text outside of JSON { } block.
+	@RequestMapping(value = "/version", method = RequestMethod.GET)
+    public Map<String, String> getAppInfo() {
+        Map<String, String> result = new HashMap<>();
+        //If tagname is empty, fall back to show the version number specified in pom.xml,which is used as git build version
+        String tag = "";
+        if (tagName.isEmpty())
+        	tag = buildVersion;
+        else
+        	tag = tagName;
+        result.put("version",tag);
+        result.put("lastcommitsha", commitSHAIdAbbrev);
+        result.put("description", "Demo to show Application version");
+        
+        LOG.info("version:" + tag + "\n" + "lastcommitsha:" + commitSHAIdAbbrev + "\n" + 
+        		"description:" + "Demo to show Application version");
+        return result;
+    }
+	
+	//Create a JSON like string to include additional text outside of JSON { } block, like "myapplication" in below case
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	 public String versionInfo() {
+		//If tagname is empty, fall back to show the version number specified in pom.xml,which is used as git build version
+	    String tag = "";
+        if (tagName.isEmpty())
+        	tag = buildVersion;
+        else
+        	tag = tagName;
+        
+		String json = "\"myapplication\": [\n";
+		json = json + " {" + "\n";
+		json += "  \"version\": " + "\"" + tag + "\"" + ",\n";
+		json += "  \"lastcommitsha\": " + "\"" + commitSHAIdAbbrev + "\"" + ",\n";
+		json += "  \"description\": " + "\"Demo to show Application version\"" + "\n";
+		json += " }" + "\n]";
+		
+        LOG.info(json);
+		return json;
+		
+	 }
 	
 }
--- src/test/java/com/se/tech/versiondemo/VersiondemoApplicationTests.java
+++ src/test/java/com/se/tech/versiondemo/VersiondemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.se.tech.versiondemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-class VersiondemoApplicationTests {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-	@Test
-	void contextLoads() {
-	}
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = VersiondemoApplication.class)
+@TestPropertySource(properties = { "spring.jmx.default-domain=test" })
+public class VersiondemoApplicationTests {
 
+    private static final Logger LOG = LoggerFactory.getLogger(VersiondemoApplicationTests.class);
+
+    @Value("${git.closest.tag.name:UNKNOWN}")
+    private String tagName;
+
+    @Value("${git.build.version:UNKNOWN}")
+    private String buildVersion;
+
+    @Value("${git.commit.id.abbrev:UNKNOWN}")
+    private String commitSHAIdAbbrev;
+
+    @Test
+    public void whenInjecting_shouldDisplay() throws Exception {
+    	LOG.info("version:" + tagName);
+        LOG.info("lastcommitsha:" + commitSHAIdAbbrev);
+        LOG.info("pom buildVersion:" + buildVersion);
+        
+        assertThat(tagName).isNotEqualTo("UNKNOWN");
+
+        assertThat(commitSHAIdAbbrev).isNotEqualTo("UNKNOWN");
+
+        assertThat(buildVersion).isNotEqualTo("UNKNOWN");
+    }
 }
